2b247c0907528dc8808ee949987c207b423b2453
salih3298 <salih3298@gmail.com>
2023-10-20 04:55:24 +0300

    CucumberReview

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @Given("user navigate to the Website")
+    public void user_navigate_to_the_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters the credentials")
+    public void user_enters_the_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("click on login button")
+    public void click_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user is logged in")
+    public void the_user_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
