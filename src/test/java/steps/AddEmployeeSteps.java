938810989c811eebfb77facf89cf1c8bac89298b
salih3298 <salih3298@gmail.com>
2023-10-31 02:41:58 +0300

    CucumberReview

--- src/test/java/steps/AddEmployeeSteps.java
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -13,28 +13,30 @@ public class AddEmployeeSteps extends CommonMethods {
         click(dashboardPage.pimButton);
 
     }
+
     @When("user clicks on add employee option")
     public void user_clicks_on_add_employee_option() {
         click(dashboardPage.addEmployeeButton);
 
     }
+
     @When("user adds multiple employees from data table")
     public void user_adds_multiple_employees_from_data_table(io.cucumber.datatable.DataTable dataTable) {
 // convert the data table as list of maps
-        List<Map<String, String>> employeesNAmes=dataTable.asMaps();
+        List<Map<String, String>> employeesNAmes = dataTable.asMaps();
 
         // iteration
-        for (Map <String,String>map:employeesNAmes) {
-            String firstname=map.get("firstName");
-            String middlename=map.get("middleName");
-            String lastname=map.get("lastName");
+        for (Map<String, String> map : employeesNAmes) {
+            String firstname = map.get("firstName");
+            String middlename = map.get("middleName");
+            String lastname = map.get("lastName");
 
             // send the names to the Webelements
             sendText(addEmployeePage.firstNameLoc, firstname);
             sendText(addEmployeePage.middleNameLoc, middlename);
             sendText(addEmployeePage.lastNameLoc, lastname);
             click(addEmployeePage.saveBtn);
-
+            click(dashboardPage.addEmployeeButton);
         }
     }
 
